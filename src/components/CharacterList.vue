37faf03ad5217598325dc25904d4cb764e88a70e
Dan HE <dan_emilie@yahoo.com>
2019-06-06 00:11:57 +0200

    Add slider to characteristics

--- src/components/CharacterList.vue
+++ src/components/CharacterList.vue
@@ -5,9 +5,38 @@
       :key="char.id"
       class="character-card card"
     >
-      <h2>{{char.name}}</h2>
-      <div>{{char.level}}</div>
-      <div>{{char.power}}</div>
+      <div class="char-name">
+        <span class="overline">Personnage</span>
+        <h2>{{char.name}}</h2>
+      </div>
+
+      <div>
+        <span class="overline">Level</span>
+        <div>{{char.level}}</div>
+      </div>
+
+      <div>
+        <span class="overline">Pouvoir</span>
+        <div>{{char.power}}</div>
+      </div>
+
+      <div>
+        <span class="overline">Experiences</span>
+        <BaseSlider
+          :value="char.current_experience"
+          :max="char.max_experience"
+          :disabled="true"
+        />
+      </div>
+
+      <div>
+        <span class="overline">Vie</span>
+        <BaseSlider
+          :value="char.current_life"
+          :max="char.max_life"
+          :disabled="true"
+        />
+      </div>
     </div>
   </div>
 </template>
@@ -15,25 +44,35 @@
 <script>
 import axios from 'axios'
 
+import BaseSlider from '@/components/core/BaseSlider.vue'
+
 export default {
   name: 'CharacterList',
+  components: {
+    BaseSlider,
+  },
+  props: {
+    selectable: {
+      type: Boolean,
+      default: false,
+    },
+  },
   data() {
     return {
       characters: [],
-      errors: []
+      errors: [],
     }
   },
   // Fetches posts when the component is created.
   created() {
-    axios.get(`https://fighting-game-api.herokuapp.com/api/v1/characters`)
-    .then(response => {
-      console.log(response)
-      // JSON responses are automatically parsed.
-      this.characters = response.data
-    })
-    .catch(e => {
-      this.errors.push(e)
-    })
+    axios.get('https://fighting-game-api.herokuapp.com/api/v1/characters')
+      .then((response) => {
+        // JSON responses are automatically parsed.
+        this.characters = response.data
+      })
+      .catch((e) => {
+        this.errors.push(e)
+      })
 
     // async / await version (created() becomes async created())
     //
@@ -43,11 +82,22 @@ export default {
     // } catch (e) {
     //   this.errors.push(e)
     // }
-  }
+  },
 }
 </script>
 
 <style lang="stylus" scoped>
   .character-list {
+    .character-card {
+      display: grid
+      grid-auto-rows: min-content
+      grid-template-columns: repeat(3, 1fr)
+      grid-row-gap: 24px
+      grid-column-gap: 16px
+
+      .char-name {
+        grid-column: 1/-1
+      }
+    }
   }
 </style>
---
+++ src/components/core/BaseSlider.vue
@@ -0,0 +1,19 @@
+<template>
+  <vue-slider v-bind="$attrs" v-on="$listeners"/>
+</template>
+
+<script>
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/antd.css";
+
+/**
+ * This component is just a wrapper for the library slider-component
+ */
+export default {
+  name: "BaseSlider",
+  inheritAttrs: false,
+  components: {
+    VueSlider
+  }
+};
+</script>
